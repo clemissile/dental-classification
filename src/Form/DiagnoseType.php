e5a3c5d2e21e0ba91bbc2d377c6159724f88f254
Clément Ganivet <ganivetclement@gmail.com>
2020-02-04 14:57:56 +0100

    Un peu de style pour nos features

--- src/Form/DiagnoseType.php
+++ src/Form/DiagnoseType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -45,7 +46,7 @@ class DiagnoseType extends AbstractType
                 ],
             ])
             ->add('dentistName', TextType::class, ['label' => 'Nom du praticien'])
-            ->add('observations', TextType::class, ['label' => 'Observations'])
+            ->add('observations', TextareaType::class, ['label' => 'Observations'])
             ->add('save', SubmitType::class, ['label' => 'Ajouter un diagnostic'])
         ;
     }
